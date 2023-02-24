92dc1077ce96f5a2a35ec9a41f7ca8153de8a70c
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-02-24 17:55:10 +0600

    all error handling is done

--- script.js
+++ script.js
@@ -13,44 +13,58 @@ function formSubmit() {
         console.log(email, password);
     } else {
         console.log(email, password);
-        alert('your input is invalid? 🤨')
+        alert('your input is invalid?')
     }
 };
 
 
 function getFloatValue(elementID) {
     const inputValue = parseFloat(document.getElementById(elementID).value);
-    document.getElementById(elementID).value ='';
-    return inputValue;
+    if (inputValue.toString().includes('-') || isNaN(inputValue)) {
+        alert('Invalid input')
+        document.getElementById(elementID).value = '';
+        return
+    }else {
+        document.getElementById(elementID).value = '';
+        return inputValue;
+    }
 };
 
 function getFloatText(elementID) {
     return parseFloat(document.getElementById(elementID).innerText);
 };
 
-function showToDOM(elementID, value2) {
-    document.getElementById(elementID).innerText = value2;
+function showToDOM(elementID, remain) {
+    document.getElementById(elementID).innerText = remain;
 }
 
 function deposit() {
     const depositAmount = getFloatValue('deposit-input');
-    const totalDeposit = getFloatText('current-deposit') + depositAmount;
-    const totalBalance = getFloatText('current-balance') + depositAmount;
-    showToDOM('current-deposit', totalDeposit);
-    showToDOM('current-balance', totalBalance);
+    if (isNaN(depositAmount)) {
+        return;
+    } else {
+        const totalDeposit = getFloatText('current-deposit') + depositAmount;
+        const totalBalance = getFloatText('current-balance') + depositAmount;
+        showToDOM('current-deposit', totalDeposit);
+        showToDOM('current-balance', totalBalance);
+    }
 };
 
 function withdraw() {
     const withdrawAmount = getFloatValue('withdraw-input');
-    const totalWithdraw = getFloatText('current-withdraw') + withdrawAmount;
-    let totalBalance = getFloatText('current-balance');
-    if (totalWithdraw > totalBalance) {
-        alert("BABA you won't get any more money than your monthly pocket expenses. Please see you next month");
-        return
+    if (isNaN(withdrawAmount)) {
+        return;
     } else {
-        totalBalance - totalWithdraw;
-    }
+        const totalWithdraw = getFloatText('current-withdraw') + withdrawAmount;
+        let totalBalance = getFloatText('current-balance');
+        if (totalWithdraw > totalBalance) {
+            alert("BABA you won't get any more money than your monthly pocket expenses. Please see you next month");
+            return
+        } else {
+            totalBalance - totalWithdraw;
+        }
 
-    showToDOM('current-withdraw', totalWithdraw);
-    showToDOM('current-balance', totalBalance);
+        showToDOM('current-withdraw', totalWithdraw);
+        showToDOM('current-balance', totalBalance);
+    }
 }
